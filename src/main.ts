086d1cf1169b1801a2f96b68d5f58a0b133bc4b3
Jonathas Marques <jonathas.souza@localiza.com>
2024-04-19 08:20:55 -0300

    alteracoes no retorno das rotas GET e POST

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,14 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
+  const cors = require('cors');
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({ transform: true, whitelist: true }));
   await app.listen(3030);
+  app.use(
+    cors({
+      origin: 'http://localhost:3000',
+    }),
+  );
 }
 bootstrap();
