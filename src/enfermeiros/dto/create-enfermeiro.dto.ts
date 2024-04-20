4b2e1d9950729af354c9bab1c21ea36559a84d73
jonathas.marques102030 <jonathascraft102030gtav@gmail.com>
2024-04-19 21:40:48 -0300

    alteracoes nas funcionalidades

--- src/enfermeiros/dto/create-enfermeiro.dto.ts
+++ src/enfermeiros/dto/create-enfermeiro.dto.ts
@@ -1,4 +1,4 @@
-import { IsEmail, IsString, isString, isStrongPassword } from 'class-validator';
+import { IsEmail, IsString } from 'class-validator';
 
 export class CreateEnfermeiroDto {
   @IsString()
--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
-  const cors = require('cors');
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe({ transform: true, whitelist: true }));
+  app.enableCors({
+    origin: 'http://localhost:3000',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true,
+});
   await app.listen(3030);
-  app.use(
-    cors({
-      origin: 'http://localhost:3000',
-    }),
-  );
 }
 bootstrap();
